7f5f0f280b481c0875e49b963aa951d301f10990
nayrasleao <nayraleao.souza@gmail.com>
2024-02-13 20:35:02 -0400

    Exercico do modulo 2

---
+++ Modulo2-Fundamentos JS/Exercicio-mod2.js
@@ -0,0 +1,85 @@
+
+//1. Resolva as operações:
+
+
+
+10 + 15 = 25
+“10” + 2 = 102
+“10” * 2 = NaN 
+“10” / 3 = NaN 
+“10” % 3 = NaN
+10 + true = 11
+10 == ”10” = True
+10 === “10” = False 
+10 < 11 = True
+10 > 12 = False
+10 <= 10.1 = True
+10 > 9.99 = True
+10 != “dez” = True
+10 + true = 11
+“dez” + true = "deztrue" 
+10 + false = 10
+10 * false = 0 
+true + true = 2 
+10++ = 11 
+10-- = 9 
+1 & 1 = True 
+1 & 0 = False
+0 & 0 = False
+1 & 0 = False
+0 / 1 = 0 
+5 + 5 == 10 = True
+“5” + ”5” == 10 = False 
+“5” * 2 > 9 = True 
+(10 + 10) * 2 = 40 
+10 + 10 * 2 = 30 
+
+
+//2. Responda as perguntas de acordo com as variáveis.
+
+
+
+var branco = “preto”; 
+
+var preto = “cinza”;
+
+var cinza = “branco”;
+
+var carro = “preto”;
+
+var valor = 30000;
+
+var prestacao = 750;
+
+
+
+a) branco == “branco” = false
+
+b) branco == cinza = false
+
+c) carro === branco = true
+
+d) var cavalo = carro == “preto” ? “cinza” : “marron”;= Marron
+
+e) Quantas prestações são necessárias para pagar o valor do carro com uma entrada
+
+de 3.000? Demonstre a operação.
+
+	var entrada = 3000
+
+	var qtdPrestacao = (valor - entrada) / prestacao
+
+    qtdPrestacao = (30000 - 3000) / 750 =  36
+
+    
+
+
+
+f) Somando as variáveis de cores é formada uma string de quantos caracteres?
+
+    var qtsCaracteres = branco+preto+cinza
+
+    resultado = qtsCaracteres.length = 13
+
+
+	
